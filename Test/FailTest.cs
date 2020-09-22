09d0e85dca126f49596257b5c9e6741af4854b3a
Piorosen <aoikazto@gmail.com>
2020-09-22 12:49:13 +0900

    feat : UnitTest 테스트

---
+++ Test/FailTest.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using hangul_utils;
+
+namespace Test
+{
+    [TestClass]
+    public class FailTest
+    {
+        [TestMethod]
+        public void TestHanguleBasis()
+        {
+            Assert.IsTrue("안녕하세요".KoreaContains("안녕하세요"), "기본적인 비교도 안됨");
+        }
+
+        [TestMethod]
+        public void TestHanguleFirst()
+        {
+            Assert.IsTrue("안녕하세요".KoreaContains("안녕"), "기본적인 비교도 안됨");
+        }
+
+        [TestMethod]
+        public void TestHanguleMiddle()
+        {
+            Assert.IsTrue("안녕하세요".KoreaContains("하세"), "기본적인 비교도 안됨");
+        }
+
+        [TestMethod]
+        public void TestHanguleLast()
+        {
+            Assert.IsTrue("안녕하세요".KoreaContains("세요"), "기본적인 비교도 안됨");
+        }
+        [TestMethod]
+        public void TestComponent()
+        {
+            Assert.IsTrue("안녕하세요".KoreaContains("ㅇㄴㅎㅅㅇ"), "기본적인 비교도 안됨");
+        }
+        [TestMethod]
+        public void TestHanguleComponentPElem()
+        {
+            Assert.IsTrue("안녕하세요".KoreaContains("안ㄴ하"), "기본적인 비교도 안됨");
+        }
+    }
+}
